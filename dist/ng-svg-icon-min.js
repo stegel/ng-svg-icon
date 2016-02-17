6ce973cc10a7ea46f666e5dae3fd8692a96409e6
AJ Siegel <aj.siegel@servicenow.com>
2016-02-17 11:47:35 -0500

    added new index page to show usage

---
+++ dist/ng-svg-icon-min.js
@@ -0,0 +1,1 @@
+!function(){"use strict";angular.module("sn.ng-svg-icon",[]).directive("snNgSvgIcon",function(){return{restrict:"E",replace:!0,scope:{icon:"=icon"},template:"<svg viewbox='0 0 100 100' id='svg-link'><use xlink:href='{{icon}}'></use></svg>"}})}();
--- ng-svg-icon.js
+++ ng-svg-icon.js
@@ -2,6 +2,13 @@
 	"use strict";
 	angular.module("sn.ng-svg-icon", []).
 		directive("snNgSvgIcon", function () {
-	
-		});
+			return {
+                restrict: 'E',
+                replace: true,
+                scope: {
+                    icon: "=icon"
+                },
+                template: "<svg viewbox='0 0 100 100' id='svg-link'><use xlink:href='{{icon}}'></use></svg>"
+            };
+        });
 }());
